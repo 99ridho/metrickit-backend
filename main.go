5bcfeea1cf01ca5de60f2b5fd464a9e1942f821f
Muhammad Ridho K. Pratama <99ridho@gmail.com>
2019-11-11 09:27:37 +0700

    load db

--- main.go
+++ main.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"fmt"
+	"github.com/99ridho/metrickit-backend/db"
+	"github.com/jmoiron/sqlx"
+	"github.com/spf13/viper"
 
 	"github.com/99ridho/metrickit-backend/handler"
 	"github.com/labstack/echo"
 )
 
+var database *sqlx.DB
+
 func main() {
+	loadConfigurationFile()
+	database = db.Initialize()
+
 	fmt.Println("Server starting...")
 
 	router := echo.New()
@@ -17,4 +25,13 @@ func main() {
 	router.POST("/payload", handler.RetrievePayload)
 
 	router.Start(":8185")
+}
+
+func loadConfigurationFile() {
+	viper.SetConfigFile("config.json")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("fatal error config file: %s", err))
+	}
 }
