bf2b8d85a4b89b4f4ed0fcab88e3511f1282db1c
Muhammad Ridho K. Pratama <99ridho@gmail.com>
2019-11-11 14:21:55 +0700

    process payload

--- handler/handler.go
+++ handler/handler.go
@@ -5,7 +5,10 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"regexp"
+	"strconv"
 
+	"github.com/99ridho/metrickit-backend/models"
 	"github.com/labstack/echo"
 )
 
@@ -35,10 +38,57 @@ func (h *Handler) RetrievePayload(c echo.Context) error {
 		})
 	}
 
-	fmt.Println(payload)
+	fmt.Println(h.extractAppLaunchMetricsTimeToFirstDrawKey(payload))
 
 	return c.JSON(http.StatusOK, map[string]interface{}{
 		"message": "success",
 		"data":    map[string]interface{}{},
 	})
+}
+
+func (h *Handler) extractAppLaunchMetricsTimeToFirstDrawKey(payloads map[string]interface{}) []*models.AppLaunchTime {
+	appLaunchMetrics := payloads["applicationLaunchMetrics"].(map[string]interface{})
+	histogrammedTimeToFirstDrawKey := appLaunchMetrics["histogrammedTimeToFirstDrawKey"].(map[string]interface{})
+	histogramValues := histogrammedTimeToFirstDrawKey["histogramValue"].(map[string]interface{})
+
+	metrics := make([]*models.AppLaunchTime, 0)
+	for i := 0; i < len(histogramValues); i++ {
+		idxStr := strconv.Itoa(i)
+		histogramValue := histogramValues[idxStr].(map[string]interface{})
+
+		frequency := int64(histogramValue["bucketCount"].(float64))
+
+		rawRangeStart := histogramValue["bucketStart"].(string)
+		rangeStartStr, _ := h.filterString(rawRangeStart, "[^0-9]+")
+		rangeStartFloat, _ := strconv.ParseFloat(rangeStartStr, 64)
+
+		rawRangeEnd := histogramValue["bucketEnd"].(string)
+		rangeEndStr, _ := h.filterString(rawRangeEnd, "[^0-9]+")
+		rangeEndFloat, _ := strconv.ParseFloat(rangeEndStr, 64)
+
+		metric := &models.AppLaunchTime{
+			HistogramValue: models.HistogramValue{
+				ID:         int64(i + 1),
+				RangeStart: rangeStartFloat,
+				RangeEnd:   rangeEndFloat,
+				Frequency:  frequency,
+			},
+		}
+
+		metrics = append(metrics, metric)
+	}
+
+	return metrics
+}
+
+func (h *Handler) filterString(str string, regex string) (string, error) {
+	reg, err := regexp.Compile(regex)
+
+	if err != nil {
+		return "", err
+	}
+
+	filteredString := reg.ReplaceAllString(str, "")
+
+	return filteredString, nil
 }
--- models/app_launchtime.go
+++ models/app_launchtime.go
@@ -1,9 +1,13 @@
 package models
 
-type AppLaunchTime struct {
+type HistogramValue struct {
 	ID         int64   `db:"id" json:"id"`
-	MetadataID int64   `db:"metadata_id" json:"metadata_id"`
 	RangeStart float64 `db:"range_start" json:"range_start"`
 	RangeEnd   float64 `db:"range_end" json:"range_end"`
 	Frequency  int64   `db:"frequency" json:"frequency"`
+}
+
+type AppLaunchTime struct {
+	HistogramValue
+	MetadataID int64 `db:"metadata_id" json:"metadata_id"`
 }
